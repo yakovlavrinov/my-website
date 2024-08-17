d594bf6f1c50d0efa8132a9a09b5a6570d7524b1
Yakov Lavrinov <lawrinow.yackov@yandex.ru>
2024-08-17 22:12:54 +0300

    js working with frames and positioning

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log('Hello world!')
+let zSpacing = -1000;
+let lastPos = zSpacing / 5;
+let $frames = document.getElementsByClassName('frame')
+let frame = Array.from($frames)
+let zVals = []
+
+window.onscroll = function() {
+    let top = document.documentElement.scrollTop
+    let delta = lastPos - top
+
+    lastPos = top
+
+    frame.forEach((n,i)=>{
+        zVals.push((i * zSpacing)+zSpacing)
+        zVals[i] += delta * -5
+        let frame = frames[i]
+        let transform = `translateZ(${zVals[i]}px)`
+
+    })
+}
